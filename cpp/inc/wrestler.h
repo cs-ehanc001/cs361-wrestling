fc133a76e34b73bf7d48de95bc5fc4897bc9df03
theomegacarrot <theomegacarrot@gmail.com>
2022-12-02 18:12:26 -0500

    wrestler data members and getters

--- cpp/inc/wrestler.h
+++ cpp/inc/wrestler.h
@@ -1,4 +1,44 @@
 #ifndef WRESTLER_H
 #define WRESTLER_H
 
+class Wrestler
+{
+private:
+
+  int m_id;
+  int m_age;
+  int m_weight;
+  int m_ability;
+
+public:
+
+  //NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+  Wrestler(int id, int age, int weight, int ability)
+      : m_id {id}
+      , m_age {age}
+      , m_weight {weight}
+      , m_ability {ability}
+  {}
+
+  [[nodiscard]] constexpr auto id() const noexcept -> int
+  {
+    return m_id;
+  }
+
+  [[nodiscard]] constexpr auto age() const noexcept -> int
+  {
+    return m_age;
+  }
+
+  [[nodiscard]] constexpr auto weight() const noexcept -> int
+  {
+    return m_weight;
+  }
+
+  [[nodiscard]] constexpr auto ability() const noexcept -> int
+  {
+    return m_ability;
+  }
+};
+
 #endif
