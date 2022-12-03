88b002651b8a2aa0056e290c876e2c73e99df1bb
theomegacarrot <theomegacarrot@gmail.com>
2022-12-02 21:14:39 -0500

    p a c k a g e !

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
+
+#include <supl/utility.hpp>
 
 auto main([[maybe_unused]] const int argc,
           [[maybe_unused]] const char* const* const argv) -> int
 {
+  std::cout << supl::to_string(std::vector {3, 5, 6, 9}) << '\n';
 
   return 0;
 }
