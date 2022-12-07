3ea9303983cf7ba201e70f71c39bedfdb02922f4
theomegacarrot <theomegacarrot@gmail.com>
2022-12-07 11:58:35 -0500

    give up

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,6 +3,14 @@
 auto main([[maybe_unused]] const int argc,
           [[maybe_unused]] const char* const* const argv) -> int
 {
+  std::cout << R"(I have absolutely no idea how this is supposed to work.
+I do not understand how a tournament or a match or a bout is supposed to work.
+I'm sure I could make it work, if I understood how it was supposed to work.
+I have spent so much time trying to understand how it should work, but I can't make heads or tails of the requirements.
+I am completely and utterly baffled, and have absolutely no idea what this program should do.
+I know nothing about how wrestling tournaments are supposed to work.
+So, I give up. I can't comprehend the requirements.
+)" << '\n';
 
   return 0;
 }
